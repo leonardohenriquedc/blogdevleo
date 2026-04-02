0a981617134dd66dfcbb2bae3371ce74e97f1694
leonardo <leonardodamascenodev@gmail.com>
2026-04-01 23:32:35 -0400

    add home page and controller to home page

---
+++ view/components/header.php
@@ -0,0 +1,4 @@
+<header class="bg-dark text-center text-white">
+    <h1><a href="index.php" class="btn btn-primary">BlogDevLeo.com</a></h1>
+    <h1><a href="index.php?to=blog&title=About">About</a></h1>
+</header>
