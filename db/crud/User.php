92be70d3fc663ee8f4772fd6657432b8c2533220
leonardo <leonardodamascenodev@gmail.com>
2026-04-02 15:50:22 -0400

    fix syntax php

--- db/crud/User.php
+++ db/crud/User.php
@@ -1,48 +1,53 @@
-<?php 
+<?php
 
-namespace Db\Crud
+namespace Db\Crud;
 
 require_once __DIR__ . "/../../config/Database.php";
-  
-use App\Model\User as UserModel
 
-class User {
-  private PDO $connection;
+use App\Model\User as UserModel;
 
-  public function __construct(){
-    %this->$connection = Database::getConnection();
-  }
+class User
+{
+    private PDO $connection;
 
-  public function findByEmail(string $email): ?UserModel
-  {
-    $stmt = $this->connection->prepare(
-        "SELECT name, email, password FROM users WHERE email = :email"
-    );
-
-    $stmt->bindParam(':email', $email);
-    $stmt->execute();
-
-    $data = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if (!$data) {
-        return null;
+    public function __construct()
+    {
+        $this->$connection = Database::getConnection();
     }
 
-    return new UserModel(
-        $data['name'],
-        $data['email'],
-        $data['password']
-    );
-  }
+    public function findByEmail(string $email): ?UserModel
+    {
+        $stmt = $this->connection->prepare(
+            "SELECT name, email, password FROM users WHERE email = :email",
+        );
 
-  public function save(Login $login): bool {
-    $stmt = $this->connection->prepare("INSERT INTO users(user, email, password) VALUES (:user, :email, :password)");
+        $stmt->bindParam(":email", $email);
+        $stmt->execute();
 
-    $stmt->bindParam([':user' => $login->user, ':email' => $login->email, ':password' => $login->password]);
-    return $stmt->execute([
-        ':user' => $login->user,
-        ':email' => $login->email,
-        ':password' => $login->password
-    ]); 
-  }
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (!$data) {
+            return null;
+        }
+
+        return new UserModel($data["name"], $data["email"], $data["password"]);
+    }
+
+    public function save(Login $login): bool
+    {
+        $stmt = $this->connection->prepare(
+            "INSERT INTO users(user, email, password) VALUES (:user, :email, :password)",
+        );
+
+        $stmt->bindParam([
+            ":user" => $login->user,
+            ":email" => $login->email,
+            ":password" => $login->password,
+        ]);
+        return $stmt->execute([
+            ":user" => $login->user,
+            ":email" => $login->email,
+            ":password" => $login->password,
+        ]);
+    }
 }
