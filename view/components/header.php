df25b53e23157e4156130415c28d7a5c420bf876
leonardo <leonardodamascenodev@gmail.com>
2026-04-03 13:44:56 -0400

    fix router of header

--- view/components/header.php
+++ view/components/header.php
@@ -1,6 +1,6 @@
 <header>
     <nav class="navbar navbar-light bg-light px-4 d-flex justify-content-around">
     <a href="index.php" class="navbar-brand fs-1">BlogDevLeo.com</a>
-    <a href="index.php?to=blog&title=About" class="navbar-link fs-1 text-decoration-none">About</a>
+    <a href="index.php?to=blog&title=about.md" class="navbar-link fs-1 text-decoration-none">About</a>
     </nav>
 </header>
