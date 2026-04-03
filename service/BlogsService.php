abd2cba3627f6de5af521e51403a20cbd6dd870a
leonardo <leonardodamascenodev@gmail.com>
2026-04-03 13:43:06 -0400

    fix bugs logic and amend renderer motor from markdown to html and add function to separator front matter of content

--- service/BlogsService.php
+++ service/BlogsService.php
@@ -2,9 +2,15 @@
 
 namespace App\Service;
 
+require_once __DIR__ . "/../vendor/autoload.php";
+
 use Exception;
 use RecursiveDirectoryIterator;
 use DirectoryIterator;
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Extension\GithubFlavoredMarkdownExtension;
+use League\CommonMark\MarkdownConverter;
 
 class BlogsService
 {
@@ -36,6 +42,59 @@ class BlogsService
         }
 
         return $titleAndfileNames;
+    }
+
+    public function getBlog(string $router): string
+    {
+        $filePath = $this->pathFiles . $router;
+
+        if (!file_exists($filePath)) {
+            throw new Exception("Blog not found");
+        }
+
+        $fileContent = file_get_contents($filePath);
+
+        $content = $this->extractContentMarkdown($fileContent);
+        $html = $this->parseDown($content);
+
+        return $html;
+    }
+
+    private function parseDown($content): string
+    {
+        // Define your configuration, if needed
+        $config = [];
+
+        // Configure the Environment with all the CommonMark and GFM parsers/renderers
+        $environment = new Environment($config);
+        $environment->addExtension(new CommonMarkCoreExtension());
+        $environment->addExtension(new GithubFlavoredMarkdownExtension());
+
+        $converter = new MarkdownConverter($environment);
+        return $converter->convert($content)->getContent();
+    }
+
+    private function extractContentMarkdown(string $markdown): string
+    {
+        $content = "";
+        $finalFrontMatter = 0;
+        $contentFile = explode("\n", $markdown);
+        foreach ($contentFile as $line) {
+            if (trim($line) === "---") {
+                $finalFrontMatter++;
+                continue;
+            }
+
+            if ($finalFrontMatter >= 2) {
+                $content .= $line . "\n";
+            }
+        }
+
+        if (trim($content) === "") {
+            throw new Exception("Invalid markdown format");
+        }
+
+        return $content;
     }
 
     private function getMetadata(string $filePath): array
