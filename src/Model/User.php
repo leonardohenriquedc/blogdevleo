234689fdebb1531c8d3832899e6623b8c96ac872
leonardo <leonardodamascenodev@gmail.com>
2026-04-06 18:07:58 -0400

    fix bug logic of namespace

--- src/Model/User.php
+++ src/Model/User.php
@@ -7,4 +7,11 @@ class User
     public string $user;
     public string $email;
     public string $password;
+
+    public function __construct(string $name, string $email, string $password)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+    }
 }
