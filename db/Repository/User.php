10c0c3a7ab8a2f214b9846e8c355bf576bb4d05f
leonardo <leonardodamascenodev@gmail.com>
2026-04-06 20:21:48 -0400

    rename folder

---
+++ db/Repository/User.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Db\Repository;
+
+require_once __DIR__ . "/../../config/Database.php";
+use Config\Database;
+use App\Model\User as UserModel;
+use PDO;
+use PDOException;
+
+class User
+{
+    private PDO $connection;
+
+    public function __construct()
+    {
+        $this->connection = Database::getConnection();
+    }
+
+    public function findByEmail(string $email): ?UserModel
+    {
+        $stmt = $this->connection->prepare(
+            "SELECT name, email, password FROM users WHERE email = :email",
+        );
+
+        $stmt->bindParam(":email", $email);
+        $stmt->execute();
+
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (!$data) {
+            return null;
+        }
+
+        return new UserModel($data["name"], $data["email"], $data["password"]);
+    }
+
+    public function save(UserModel $login): bool
+    {
+        $stmt = $this->connection->prepare(
+            "INSERT INTO users(user, email, password) VALUES (:user, :email, :password)",
+        );
+
+        $stmt->bindParam([
+            ":user" => $login->user,
+            ":email" => $login->email,
+            ":password" => $login->password,
+        ]);
+        return $stmt->execute([
+            ":user" => $login->user,
+            ":email" => $login->email,
+            ":password" => $login->password,
+        ]);
+    }
+}
